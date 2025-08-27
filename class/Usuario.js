6051ebf595ab064bdad7659ffce9b10e0e669a0e
♡ Rosiele Sousa ♡ <rosielesousa49@gmail.com>
2025-08-27 08:37:55 -0300

    Add files via upload

---
+++ class/Usuario.js
@@ -0,0 +1,35 @@
+class Usuario {
+    static totalUsuarios = 0
+    #ativo
+    constructor(nome, idade,email) {
+        this.nome = nome
+        this.idade = idade
+        this.email = email
+        this.#ativo = false
+        Usuario.totalUsuarios++
+    }
+
+    get boasVindas() {
+        return `Bem-vindo, ${this.nome}!`
+    }
+    get ativo() {
+        return this.#ativo
+    }
+    set ativo(valor) {
+        if(typeof valor === 'boolean') {
+            this.#ativo = valor
+        } else{
+            'Usuário não ativo.'
+        }
+    }
+    mostrarDados(){
+        console.log (`
+            Nome: ${this.nome}
+            Idade: ${this.idade}
+            Email: ${this.email}
+            Ativo: ${this.ativo}`)
+    }
+}
+module.exports = Usuario
+
+//toLocaleUpperCase()
---
+++ index.js
@@ -0,0 +1,45 @@
+const prompt = require('prompt-sync')()
+const Usuario = require('./class/Usuario')
+
+const usuarios = []
+let continuar = true
+
+while(continuar){
+    const nome = prompt('Digite seu nome: ')
+    const idade = Number(prompt('Digite sua idade: '))
+    const email = prompt('Digite seu email: ')
+    
+    const user = new Usuario(nome, idade, email)
+
+    const ativo = (prompt('Usuário ativo? [S/N]: '))
+    if(ativo === 'S' || ativo === 's'){
+        user.ativo = true
+    }else if(ativo === 'N' || ativo === 'n'){
+        user.ativo = false
+    } else{
+        console.log('Dado incorreto!')
+        break
+    }
+
+    usuarios.push(user)
+
+    const adicionarUsuario = prompt('Deseja adicionar outro usuário? [S/N]: ')
+    if(adicionarUsuario === 'S' || adicionarUsuario === 's'){
+        user.adicionarUsuario = true 
+    } else if(adicionarUsuario === 'N' || adicionarUsuario === 'n'){
+        user.adicionarUsuario = false
+    } else{
+        console.log('Dado incorreto!')
+        break
+    }
+    for (let i = 0; i < usuarios.length; i++){
+        usuarios[i].mostrarDados()
+    if(i < usuarios.length -1){
+        console.log(`
+            =====================`)
+        }
+    }
+}
+if(usuarios.length > 0){
+    Usuario.totalUsuarios()
+}
